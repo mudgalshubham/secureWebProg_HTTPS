1584fdef42bbf395326e24647ff2a0a86d41ca2b
Shubham Mudgal <shubham.mudgal@colorado.edu>
2016-03-13 21:15:16 -0600

    updated whitelistIPs

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 echo "<html><head></head><body>
  		<div align=center><a href=index.php>Story List|</a>
  		<a href=index.php?s=4>Character List|</a>
- 		<a href=add.php?s=4>Add Characters</a></div><hr>
+ 		<a href=add.php?s=5>Add Characters</a></div><hr>
   		<div align=center>";
   		
   		
