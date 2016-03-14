9438337ef53039d0e81c1239cf5ff147787ffd06
Shubham Mudgal <shubham.mudgal@colorado.edu>
2016-03-13 22:07:57 -0600

    updated whitelistIPs

--- add.php
+++ add.php
@@ -64,7 +64,8 @@ function addCharacterMenu($s, $attemptCount, $whiteListIPAddress)
 					echo "whitelist = $whiteListIPAddress[$i]";
 				}
 				$isWhiteListIP = in_array($IPAddress,$whiteListIPAddress);
-				echo "is it there = ".$isWhiteListIP;
+				if($isWhiteListIP)
+					echo "is it there = ".$isWhiteListIP;
 				
 				if(is_numeric($s)) addCharacterForm(); break;
 
